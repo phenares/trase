d7fd6d1ff1f138187f7fee8c2555240e81b52104
Alvaro Leal <pursuance@gmail.com>
2019-02-14 23:16:20 +0100

    Move docs to main folder

--- frontend/doczrc.js
+++ frontend/doczrc.js
@@ -3,8 +3,11 @@ import merge from 'webpack-merge';
 import webpackConfig from './config/webpack.config';
 
 export default {
+  src: '../doc/trase',
   dest: './docs/dist',
   title: 'Trase',
+  files: '**/*.{md,markdown,mdx}',
+  menu: ['Home', { name: 'Components', menu: ['cards', 'button'] }],
   themeConfig: {
     colors: {
       primary: '#34444C',
