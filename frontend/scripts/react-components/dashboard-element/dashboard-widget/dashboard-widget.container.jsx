da84371d483372a421e1b0dab622129fefb88fa7
Alvaro Leal <pursuance@gmail.com>
2019-04-01 20:38:08 +0200

    Create Table modal action and component

--- frontend/scripts/react-components/dashboard-element/dashboard-widget/dashboard-widget.container.jsx
+++ frontend/scripts/react-components/dashboard-element/dashboard-widget/dashboard-widget.container.jsx
@@ -4,14 +4,20 @@ import PropTypes from 'prop-types';
 import DashboardWidgetComponent from 'react-components/dashboard-element/dashboard-widget/dashboard-widget.component';
 import DashboardWidgetTooltip from 'react-components/dashboard-element/dashboard-widget/dashboard-widget-tooltip';
 import { makeGetConfig } from 'react-components/dashboard-element/dashboard-widget/dashboard-widget.selectors';
+import { setDashboardWidgetActiveModal } from 'react-components/dashboard-element/dashboard-widget/dashboard-widget.actions';
 
 const makeMapStateToProps = () => {
   const getDashboardWidgetsConfig = makeGetConfig();
   const mapStateToProps = (state, props) => ({
-    config: getDashboardWidgetsConfig(state, props)
+    config: getDashboardWidgetsConfig(state, props),
+    activeModal: state.dashboardWidget.activeModal && state.dashboardWidget.activeModal.modal
   });
   return mapStateToProps;
 };
+
+const mapDispatchToProps = dispatch => ({
+  setActiveModal: modal => dispatch(setDashboardWidgetActiveModal({ modal }))
+});
 
 class DashboardWidgetContainer extends Component {
   addTooltipContentToConfig(config, meta) {
@@ -25,7 +31,7 @@ class DashboardWidgetContainer extends Component {
   }
 
   render() {
-    const { data, loading, error, meta, title, config } = this.props;
+    const { data, loading, error, meta, title, config, setActiveModal, activeModal } = this.props;
     return config ? (
       <DashboardWidgetComponent
         data={data}
@@ -33,6 +39,8 @@ class DashboardWidgetContainer extends Component {
         error={error}
         loading={loading}
         chartConfig={this.addTooltipContentToConfig(config, meta)}
+        setActiveModal={setActiveModal}
+        activeModal={activeModal}
       />
     ) : null;
   }
@@ -44,7 +52,12 @@ DashboardWidgetContainer.propTypes = {
   meta: PropTypes.object,
   loading: PropTypes.bool,
   title: PropTypes.string,
-  config: PropTypes.object
+  config: PropTypes.object,
+  setActiveModal: PropTypes.func.isRequired,
+  activeModal: PropTypes.string
 };
 
-export default connect(makeMapStateToProps)(DashboardWidgetContainer);
+export default connect(
+  makeMapStateToProps,
+  mapDispatchToProps
+)(DashboardWidgetContainer);
